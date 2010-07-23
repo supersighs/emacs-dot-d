43f75b3eab9cbac01b9be9b39161f95c4b0338d7
Tommy Klumker <tommy@smithandrobot.com>
2010-07-23 15:37:00 -0500

    moved mmmode

--- tommy-stuff.el
+++ tommy-stuff.el
@@ -1,6 +1,7 @@
 (load-library "tommy-theme")
 
-(add-to-list 'load-path (concat dotfiles-dir "/mmm-mode"))
-(add-to-list 'load-path (concat dotfiles-dir "/Pymacs"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/mmm-mode"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/Pymacs"))
 
 (load-library "mako")
+(load-library "tommy-python")
